6a2f62aeb7bd3b8b701d4e0e058a6e76d333eee9
You Youngbai <nawhizz@gmail.com>
2019-04-02 21:37:39 +0900

    blog.Post 모델에 대한 CRUD 구현

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -6,6 +7,12 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
 
 
 class Comment(models.Model):
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new/', views.post_new, name='post_new'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
